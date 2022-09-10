d3e03c80241a22a3e5693c5a8bcd569a30e2a2d7
Joshua Musa <gidijosh@gmail.com>
2022-09-10 13:45:03 +0100

    fix order issue and quantity issue

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -31,20 +31,16 @@ export const ShopProvider = ({ children }) => {
     let duplicate;
     let cart = { title, price, image, productId, quantity: 1 };
 
-    if (cartItems.length === 0) {
-      setCartItems([...cartItems, cart]);
-    } else if (cartItems.length > 0) {
-      duplicate = cartItems.findIndex((item) => item.productId === productId);
-      let duplicateArr = JSON.parse(JSON.stringify(cartItems));
+    duplicate = cartItems.findIndex((item) => item.productId === productId);
+    let duplicateArr = JSON.parse(JSON.stringify(cartItems));
 
-      if (duplicate !== -1) {
-        cart.quantity += 1;
-        duplicateArr.splice(duplicate, 1, cart);
-        setCartItems(duplicateArr);
-      } else {
-        setCartItems([...cartItems, cart]);
-      }
+    if (duplicate !== -1) {
+      cart.quantity = cartItems[duplicate].quantity + 1;
+      duplicateArr.splice(duplicate, 1, cart);
+    } else {
+      duplicateArr.push(cart);
     }
+    setCartItems(duplicateArr);
   };
 
   const handleSearchChange = (e) => {
@@ -58,30 +54,32 @@ export const ShopProvider = ({ children }) => {
   };
 
   const handleOrderCompleted = (cartItems) => {
-    // let products = cartItems.map(({ productId, quantity }) => ({
-    //   productId,
-    //   quantity,
-    // }));
+    let products = cartItems.map(({ productId, quantity }) => ({
+      productId,
+      quantity,
+    }));
 
-    // const addOrder = async () => {
-    //   const res = await fetch("https://fakestoreapi.com/carts", {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       userId: uuidv4(),
-    //       date: today,
-    //       products: products,
-    //     }),
-    //   });
-    //   const data = await res.json();
-    //   console.log(data);
-    //   setOrders([...orders, data]);
-    // };
+    const addOrder = async () => {
+      const res = await fetch("https://fakestoreapi.com/carts", {
+        method: "POST",
+        body: JSON.stringify({
+          userId: uuidv4(),
+          date: today,
+          products: products,
+        }),
+      });
+      const data = await res.json();
 
-    // addOrder();
+      // Data is an object with a property id which is always equals 11
+      // so instead of using the ID, i would use UUIV to generate a new order ID instead
+      console.log(data);
+      // setOrders([...orders, data]);
+      let orderId = uuidv4();
+      setOrders([...orders, { cartItems, orderId }]);
+      setCartItems([]);
+    };
 
-    let orderId = uuidv4();
-    setOrders([...orders, { cartItems, orderId }]);
-    setCartItems([]);
+    addOrder();
   };
 
   return (
